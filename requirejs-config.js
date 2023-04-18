d216149f7c5e50857dda67deb25de8505be1eacc
Wafula James <33344724+Jameswafy@users.noreply.github.com>
2023-04-18 16:20:37 +0300

    Update requirejs-config.js

--- requirejs-config.js
+++ requirejs-config.js
@@ -8,10 +8,7 @@
 var config = {
     map: {
         '*': {
-            'rowBuilder':             'Magento_Theme/js/row-builder',
-            'toggleAdvanced':         'mage/toggle',
-            'translateInline':        'mage/translate-inline',
-            'sticky':                 'mage/sticky',
+           
             'tabs':                   'mage/tabs',
             'zoom':                   'mage/zoom',
             'collapsible':            'mage/collapsible',
